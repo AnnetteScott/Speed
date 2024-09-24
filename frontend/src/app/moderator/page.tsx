9fb5abca949929109c0fa7eb74ecf4876510adff
Yash <yashsavani19@gmail.com>
2024-09-24 21:28:35 +1200

    added moderator page as home, for quick testing

---
+++ frontend/src/app/moderator/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModeratorPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Moderator Page</h1>
+            <p>Welcome to the moderator page.</p>
+        </div>
+    );
+};
+
+export default ModeratorPage;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from '../components/navBar'
 
+import ModeratorPage from "../app/moderator/page";
+
 export default function Home() {
 	return (
 		<main>
 			<NavBar />
+
+		{/* TEMPORARY */}
+		<ModeratorPage/>
+		{/* TEMPORARY */}
+			
 		</main>
 	);
 }
