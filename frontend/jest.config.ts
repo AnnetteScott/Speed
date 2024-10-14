009e49b45bda2a421a6d5b56fcab31d3275f94b6
Yash <yashsavani19@gmail.com>
2024-10-14 18:16:09 +1300

    Resolved error of app not being able to build, by seperating tsconfig file for tests and running, and also adding next.config file

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -6,6 +6,11 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json', 
+    },
+  },
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
